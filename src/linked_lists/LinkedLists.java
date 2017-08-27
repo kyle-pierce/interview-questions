e563b73b3dfc9759367a0d721d1e67c01bfc5ab3
Kyle Pierce <pierce.kyle1996@gmail.com>
2017-08-27 13:32:41 -0700

    Lists -> Sum Problems and Tests

--- src/linked_lists/LinkedLists.java
+++ src/linked_lists/LinkedLists.java
@@ -43,7 +43,7 @@ public class LinkedLists {
 	
 	/* Returns the node at the kth to last index in the list beginning
 	 * with the given Node. */
-	public static Node removeKthToLast(int k, Node front) {
+	public static Node returnKthToLast(int k, Node front) {
 		Node toEnd = front;
 		Node toReturned = front;
 		
@@ -75,7 +75,7 @@ public class LinkedLists {
 	/* Partitions the list beginning with the given node around the given value
 	 * x such that all values less than x come first, then all values equal to
 	 * x, then all values greater than x. */
-	public static void partitionAround(Node front, int x) {
+	public static Node partitionAround(Node front, int x) {
 		// initialize the needed pointers
 		Node smallerFront = null; Node smallerBack = null;
 		Node equalFront = null; Node equalBack = null;
@@ -87,20 +87,33 @@ public class LinkedLists {
 			front.next = null;
 			if (front.data < x) {
 				// add to the smaller list
-				smallerFront = (smallerFront == null) ? front : smallerFront;
-				smallerBack.next = front;
-				smallerBack = front;
+				if (smallerFront == null) {
+					smallerBack = front;
+					smallerFront = front;
+				} else {
+					smallerBack.next = front;
+					smallerBack = front;
+				}
 			} else if (front.data == x) {
 				// add to the equal list
-				equalFront = (equalFront == null) ? front : equalFront;
-				equalBack.next = front;
-				equalBack = front;
+				if (equalFront == null) {
+					equalBack = front;
+					equalFront = front;
+				} else {
+					equalBack.next = front;
+					equalBack = front;
+				}
 			} else {
 				// add to the greater list
-				greaterFront = (greaterFront == null) ? front : greaterFront;
-				greaterBack.next = front;
-				greaterBack = front;
+				if (greaterFront == null) {
+					greaterBack = front;
+					greaterFront = front;
+				} else {
+					greaterBack.next = front;
+					greaterBack = front;
+				}
 			}
+			front = next;
 		}
 		
 		// set the back of the 3rd list to null if not already
@@ -119,19 +132,78 @@ public class LinkedLists {
 				// there are equal nodes
 				// set front to equal elements followed by larger elements
 				front = equalFront;
-				equalBack.next = greaterBack;
+				equalBack.next = greaterFront;
 			}
 		} else {
 			// there are some smaller nodes
 			front = smallerFront;
 			if (equalFront == null) {
 				// there are no equal nodes, just smaller and greater
-				smallerBack.next = greaterBack;
+				smallerBack.next = greaterFront;
 			} else {
 				// there are all 3 classifications of elements
 				smallerBack.next = equalFront;
 				equalBack.next = greaterFront;
 			}
 		}
+		return front;
+	}
+	
+	/* Given two lists representing numbers stored in reverse order,
+	 * returns the list that is the reverse of the sum of those
+	 * two numbers.   */
+	public static Node reverseSumLists(Node list1, Node list2) {
+		return numberToListReverse(listToNumberReverse(list1, 1) + 
+								   listToNumberReverse(list2, 1));
+	}
+	
+	/* Converts a list to a number in reverse. */
+	private static int listToNumberReverse(Node front, int factor) {
+		if (front == null) {
+			return 0;
+		} else {
+			return factor * front.data + listToNumberReverse(front.next, factor * 10);
+		}
+	}
+	
+	/* Converts a number to list in reverse. */
+	private static Node numberToListReverse(int number) {
+		if (number == 0) {
+			return null;
+		} else {
+			Node node = new Node(number % 10, numberToListReverse(number / 10));
+			return node;
+		}
+	}
+	
+	/* Returns a list which is the sum of the two given lists. */
+	public static Node sumLists(Node list1, Node list2) {
+		return numberToList(listToNumber(list1) + listToNumber(list2));
+	}
+	
+	/* Converts the list beginning with the given node to a number. */
+	private static int listToNumber(Node front) {
+		StringBuilder builder = new StringBuilder();
+		
+		Node current = front;
+		while (current != null) {
+			builder.append(current.data);
+			current = current.next;
+		}
+		
+		return Integer.parseInt(builder.toString());
+	}
+	
+	/* Returns a list representing the given number. */
+	private static Node numberToList(int number) {
+		String digits = String.valueOf(number);
+		
+		Node front = null;
+		for (int i = digits.length() - 1; i >= 0; i--) {
+			// 48 is the ASCII value for 0
+			front = new Node(digits.charAt(i) - 48, front);
+		}
+		
+		return front;
 	}
 }
--- src/linked_lists/Tests.java
+++ src/linked_lists/Tests.java
@@ -26,11 +26,78 @@ public class Tests {
 		assertTrue(listsAreEqual(list, after));
 	}
 	
+	@Test
+	public void testRemoveKToLast() {
+		Node list = initializeListFromArray(new int[] {1, 2, 3, 4, 5, 6, 7});
+		
+		Node returned = LinkedLists.returnKthToLast(3, list);
+		assertSame(5, returned.data);
+		
+		returned = LinkedLists.returnKthToLast(1, list);
+		assertSame(7, returned.data);
+		
+		returned = LinkedLists.returnKthToLast(7, list);
+		assertSame(1, returned.data);
+	}
+	
+	@Test
+	public void testRemoveMiddle() {
+		Node list = initializeListFromArray(new int[] {1, 2, 3, 4, 5});
+		Node listAfter = initializeListFromArray(new int[] {1, 2, 4, 5});
+		
+		Node toBeRemoved = list.next.next;
+		
+		LinkedLists.removeMiddleNode(toBeRemoved);
+		
+		assertTrue(listsAreEqual(listAfter, list));
+	}
+	
+	@Test
+	public void testPartitionAroundValuePresent() {
+		Node list = initializeListFromArray(new int[] {5, 4, 3, 2, 1});
+		Node listAfter = initializeListFromArray(new int[] {2, 1, 3, 5, 4});
+		
+		list = LinkedLists.partitionAround(list, 3);
+		
+		assertTrue(listsAreEqual(listAfter, list));
+	}
+	
+	@Test
+	public void testPartitionAroundValueNotPresent() {
+		Node list = initializeListFromArray(new int[] {5, 2, 6, 1, 0, 4});
+		Node listAfter = initializeListFromArray(new int[] {2, 1, 0, 5, 6, 4});
+		
+		list = LinkedLists.partitionAround(list, 3);
+		assertTrue(listsAreEqual(listAfter, list));
+	}
+	
+	@Test
+	public void testReverseSumLists() {
+		Node list1 = initializeListFromArray(new int[] {7, 1, 6});
+		Node list2 = initializeListFromArray(new int[] {5, 9, 2});
+		Node expected = initializeListFromArray(new int[] {2, 1, 9});
+		
+		Node observed = LinkedLists.reverseSumLists(list1, list2);
+		
+		assertTrue(listsAreEqual(expected, observed));
+	}
+	
+	@Test
+	public void testSumLists() {
+		Node list1 = initializeListFromArray(new int[] {6, 1, 7});
+		Node list2 = initializeListFromArray(new int[] {2, 9, 5});
+		Node expected = initializeListFromArray(new int[] {9, 1, 2});
+		
+		Node observed = LinkedLists.sumLists(list1, list2);
+		
+		assertTrue(listsAreEqual(expected, observed));
+	}
+	
 	/* Returns true if the two lists beginning with the given nodes are equal
 	 * and false otherwise. */
-	private boolean listsAreEqual(Node front1, Node front2) {
-		Node current1 = front1;
-		Node current2 = front2;
+	private boolean listsAreEqual(Node expected, Node observed) {
+		Node current1 = expected;
+		Node current2 = observed;
 		
 		// compare values in the two lists
 		while (current1 != null && current2 != null) {
@@ -59,5 +126,4 @@ public class Tests {
 		}
 		return front;
 	}
-
 }
