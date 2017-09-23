e6e30b46f6f7fc7c934ba1c146ce1dd144c1026a
Kyle Pierce <pierce.kyle1996@gmail.com>
2017-09-23 15:35:01 -0700

    Start of RandomBinaryTree

---
+++ src/trees_and_graphs/RandomBinaryTree.java
@@ -0,0 +1,34 @@
+package trees_and_graphs;
+
+public class RandomBinaryTree<E> {
+	private int size;
+	private TreeNode<E> root;
+	
+	/**
+	 * Constructs an empty RandomBinaryTree
+	 */
+	public RandomBinaryTree() {
+		size = 0;
+		root = null;
+	}
+	
+	/**
+	 * Adds the given element at the location nearest to the overall
+	 * root of this tree.
+	 * 
+	 * @param element element to be added to this tree
+	 */
+	public void insert(E element) {
+		
+	}
+	
+	/**
+	 * Removes all occurrences of the given element from this tree,
+	 * replacing it with its left child.
+	 * 
+	 * @param element element to be removed
+	 */
+	public void remove(E element) {
+		
+	}
+}
